04b490f43404e0fe0a773defe0e55d93d5f70410
Henrique-de-Souza-Figueiredo <henriquesf2009@gmail.com>
2025-05-09 11:11:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, redirect, render_template, flash
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 app.secret_key = 'chave'
 
+emprestados=[]
 livros=[]
-emprestado=[]
 
 @app.route('/')
 def index():
@@ -16,8 +18,9 @@ def adicionar_livro():
         nome= request.form['nome']
         autor = request.form['autor']
         ano = request.form['ano']
+
         codigo= len(livros)
-        livros.append([codigo, nome, autor, ano])
+        livros.append([codigo, nome, autor, ano, ])
         flash('Livro Adicionado')
         return redirect('/')
     else:
@@ -29,7 +32,7 @@ def editar_livro(codigo):
         nome = request.form['nome']
         autor = request.form['autor']
         ano = request.form['ano']
-        livros[codigo] = [codigo, nome, autor, ano]
+        livros[codigo] = [codigo, nome, autor, ano,]
         return redirect('/')
     else:
         livro = livros[codigo]
@@ -40,16 +43,32 @@ def apagar_livro(codigo):
     del livros[codigo]
     return redirect('/')
 
-@app.route('/emprestar_livro/<int:codigo>')
+@app.route("/emprestar_livro/<int:codigo>")
 def emprestar_livro(codigo):
-    if request.method == 'POST':
-        flash('Livro Emprestado')
-        emprestado.append([livros[codigo]])
-        return redirect('/')
-    else:
-        return render_template('livros_emprestados.html')
+    hoje = datetime.now()
+    devolucao = hoje + timedelta(days=-7)
+    livros[codigo]["emprestado"] = True
+    livros[codigo]["data_emprestimo"] = hoje
+    livros[codigo]["data_devolucao"] = devolucao
+
+    flash("Livro emprestado! Devolução até " + devolucao.strftime("%d/%m/%Y"))
+    return redirect("/")
+
+
+
+@app.route("/devolver/<int:codigo>")
+def devolver(codigo):
+    hoje = datetime.now()
+    devolucao = livros[codigo]["data_devolucao"]
+    atraso = (hoje - devolucao).days
+    if atraso > 0:
+        multa = 10 + (10 * 0.01 * atraso)
+        flash(f"Livro devolvido com {atraso} dias de atraso. multa: R$ {multa}")
+    return redirect("/")
 
 
 
 if __name__=='__main__':
     app.run(debug=True)
+
+
